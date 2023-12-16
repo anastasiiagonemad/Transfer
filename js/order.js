5a4b271230534d986eb60626e8969542ca72d6f8
Elena Sinelnikova <elle.sinelle@gmail.com>
2023-12-16 16:45:34 +0300

    feat: insert pic in page with js

--- js/order.js
+++ js/order.js
@@ -4,7 +4,7 @@ let passengers = `
 <div class="">
     <div>blablabla</div>
     <div class="popup__container">
-    <img class="img" src='../../assets/image/driver.jpg' alt="driver">
+    <img class="img" src='../../assets/image/driver.jpg' alt="driver" width="350">
     </div>
 </div>
 `;
@@ -13,7 +13,16 @@ let cargo = `
 <div class="">
     <div>xoxoxo</div>
     <div class="popup__container">
-    <img class="img" src='../../assets/image/nav.jpg' alt="nav">
+    <img class="img" src='../../assets/image/nav.jpg' alt="nav" >
     </div>
 </div>
 `;
+
+let box = document.querySelector('.top-container__order-box');
+
+window.addEventListener('load', () => {
+  let block = document.createElement('div');
+  block.className = 'block';
+  block.innerHTML = passengers;
+  box.append(block);
+});
