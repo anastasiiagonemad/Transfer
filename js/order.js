d443eeb811a9d6efebeb533cee1f5fe5ca7d6b7e
Elena Sinelnikova <elle.sinelle@gmail.com>
2023-12-16 16:30:06 +0300

    feat: add file order.js

---
+++ js/order.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let passengers = `
+<div class="">
+    <div>blablabla</div>
+    <div class="popup__container">
+    <img class="img" src='../../assets/image/driver.jpg' alt="driver">
+    </div>
+</div>
+`;
+
+let cargo = `
+<div class="">
+    <div>xoxoxo</div>
+    <div class="popup__container">
+    <img class="img" src='../../assets/image/nav.jpg' alt="nav">
+    </div>
+</div>
+`;
