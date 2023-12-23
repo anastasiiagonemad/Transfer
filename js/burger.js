36ced81894ca30fc173b3c83fe99c685aafecdc3
Elena Sinelnikova <elle.sinelle@gmail.com>
2023-12-23 15:32:20 +0300

    feat: start work on burger

--- js/burger.js
+++ js/burger.js
@@ -1,37 +1,57 @@
 'use strict'
 
-const menu = document.querySelector('.header__nav');
+let burger = document.querySelector('.burger');
 let burgerBtn = document.querySelector('.header__burger');
-const links = document.querySelectorAll('.nav-menu__link');
+let links = document.querySelectorAll('.burger__nav-item');
 let body = document.body;
 let fade = document.querySelector('.fade');
 
 let burgerMenu = `
-  <div class="form-container">
-  <form class="form form_passengers" name="passengers">
-    <div class="form__dest">
-      <div class="departure">Германия</div>
-      <img class="reverse-btn" src="assets/icons/exchange.svg">
-      <div class="arrival">Калининград</div>
-    </div>
-`
+  <div class="burger_container">
+    <div class="burger__close><img class="assets/icons/close-icon.svg"</div>
+    <nav class="burger__nav">
+      <ul class="burger__nav-container">
+        <li><a class="burger__nav-item" href="#">Направления</a></li>
+        <li><a class="burger__nav-item" href="#">Услуги</a></li>
+        <li><a class="burger__nav-item" href="#">Преимущества</a></li>
+        <li><a class="burger__nav-item" href="#">Отзывы</a></li>
+        <li><a class="burger__nav-item" href="#">Вопросы и ответы</a></li>
+      </ul>
+    </nav>
+  </div>
+`;
 
-function toggleBurgerMenu() {
-    menu.classList.toggle('active');
-    burgerBtn.classList.toggle('active');
-    body.classList.toggle('lock');
-    fade.classList.toggle('active');
+
+function createBurger() {
+
+    let burgerBlock = document.createElement('div');
+    // burgerBlock.className = 'burger_container';
+    burgerBlock.innerHTML = burgerMenu;
+    burger.append(burgerBlock);
+    console.log(burgerBlock);
+
 }
 
-function closeBurgerMenu() {
-    menu.classList.remove('active');
-    burgerBtn.classList.remove('active');
-    body.classList.remove('lock');
-    fade.classList.remove('active');
-}
-
-burgerBtn.addEventListener('click', toggleBurgerMenu);
-fade.addEventListener('click', closeBurgerMenu);
-links.forEach(link => {
-    link.addEventListener('click', closeBurgerMenu);
+burgerBtn.addEventListener('click', function() {
+  createBurger();
 })
+
+// function toggleBurgerMenu() {
+//     menu.classList.toggle('active');
+//     burgerBtn.classList.toggle('active');
+//     body.classList.toggle('lock');
+//     fade.classList.toggle('active');
+// }
+
+// function closeBurgerMenu() {
+//     menu.classList.remove('active');
+//     burgerBtn.classList.remove('active');
+//     body.classList.remove('lock');
+//     fade.classList.remove('active');
+// }
+
+// burgerBtn.addEventListener('click', toggleBurgerMenu);
+// fade.addEventListener('click', closeBurgerMenu);
+// links.forEach(link => {
+//     link.addEventListener('click', closeBurgerMenu);
+// })
